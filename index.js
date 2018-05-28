a74399d7311e1bd399a7efab3249a45919377ed5
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 19:36:54 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-import reversal from "@unction/reversal"
-import selectByValue from "@unction/selectbyvalue"
-
-import type {PredicateFunctionType} from "types"
-import type {UnaryFunctionType} from "types"
-import type {EnumerableType} from "types"
-
-export default function rejectByValue (predicate: PredicateFunctionType): UnaryFunctionType {
-  return function rejectByValuePredicate (enumerable: EnumerableType): EnumerableType {
+import reversal from "@unction/reversal";
+import selectByValue from "@unction/selectbyvalue";
+export default function rejectByValue(predicate) {
+  return function rejectByValuePredicate(enumerable) {
     if (enumerable.reject) {
-      return enumerable.reject(predicate)
+      return enumerable.reject(predicate);
     }
 
-    return selectByValue(reversal(predicate))(enumerable)
-  }
+    return selectByValue(reversal(predicate))(enumerable);
+  };
 }
